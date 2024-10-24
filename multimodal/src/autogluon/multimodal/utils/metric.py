5c8e88d7500de343fa61cd420c4161fdf3985e4e
mtdiedrich <mitchdiedrich@gmail.com>
2024-10-23 21:27:15 -0500

    Fix log loss calculation for `MultiModalPredictor.evaluate()`.
    Log loss is calculated using probabilities. The existing implementation instead uses labels. This change corrects the existing implementation to use probabilities.

--- multimodal/src/autogluon/multimodal/utils/metric.py
+++ multimodal/src/autogluon/multimodal/utils/metric.py
@@ -219,7 +219,7 @@ def compute_score(
     -------
     Computed score.
     """
-    if isinstance(metric, str) and metric in [OVERALL_ACCURACY, OVERALL_F1]:
+    if isinstance(metric, str) and metric in [OVERALL_ACCURACY, OVERALL_F1, LOG_LOSS]:
         metric = evaluate.load("seqeval")
         warnings.filterwarnings("ignore")
         for p in metric_data[Y_TRUE]:
@@ -233,6 +233,8 @@ def compute_score(
     metric = get_metric(metric)
     if metric.name in [ROC_AUC, AVERAGE_PRECISION]:
         return metric._sign * metric(metric_data[Y_TRUE], metric_data[Y_PRED_PROB][:, pos_label])
+    elif metric.name in [LOG_LOSS]:
+        return metric._sign * metric(metric_data[Y_TRUE], metric_data[Y_PRED_PROB])
     elif metric.name in [F1]:  # only for binary classification
         return f1_score(metric_data[Y_TRUE], metric_data[Y_PRED], pos_label=pos_label)
     else:
